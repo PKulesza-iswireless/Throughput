9f4464942493ceb99fafa8a5abaa2ab386b987a7
PiotrKulesza <piotrkulesza16@onet.pl>
2020-08-10 17:34:36 +0200

    Edit MetricProperties

--- src/main/java/sensor/common/MetricProperties.java
+++ src/main/java/sensor/common/MetricProperties.java
@@ -237,6 +237,7 @@ public class MetricProperties {
                         if (EnumUtils.isValidEnum(ApplicationName.class, applicationName)) {
                             ApplicationName applicationNameEnum = EnumUtils.getEnum(ApplicationName.class, applicationName);
                             switch (applicationNameEnum) {
+                            	case TEST:
                                 case FCR:
                                 case GENOM:
                                 case CE_TRAFFIC:
